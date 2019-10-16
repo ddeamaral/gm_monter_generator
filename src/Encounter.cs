afc3e6014ca7dda88d3703f6795fea3b6cd66b84
Dylan DeAmaral <deamaral.dylan@gmail.com>
2019-10-16 19:32:35 -0400

    Refactors structs to separate files (cleanliness)

---
+++ src/Encounter.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace src
+{
+    public struct Encounter
+    {
+        public Dictionary<string, int> Monsters;
+
+        public decimal AdjustedExperience;
+    }
+}
